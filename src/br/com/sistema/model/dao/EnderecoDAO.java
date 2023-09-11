40a8766d4a85314e3be311aa951bd3d43173135f
WesleySouzaSilva <wesley_souza19982010@hotmail.com>
2023-09-10 23:59:46 -0300

    test : criacao classe EnderecoTest

--- src/br/com/sistema/model/dao/EnderecoDAO.java
+++ src/br/com/sistema/model/dao/EnderecoDAO.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.sistema.conexao.AbstractGenericDAO;
@@ -19,96 +20,102 @@ public class EnderecoDAO extends AbstractGenericDAO<Endereco> {
 
 	@Override
 	public boolean inserir(Endereco pojo) {
-		String sqlEndereco = "INSERT INTO endereco(rua, bairro, numero, cidade, estado, cep) VALUES (?, ?, ?, ?, ?, ?) ";
-		try {
-			PreparedStatement stmt = dbConnection.prepareStatement(sqlEndereco);
-			stmt.setString(1, pojo.getRua());
-			stmt.setString(2, pojo.getBairro());
-			stmt.setString(3, pojo.getNumero());
-			stmt.setString(4, pojo.getCidade());
-			stmt.setString(5, pojo.getUf());
-			stmt.setString(6, pojo.getCep());
-			int retornoEnd = stmt.executeUpdate();
+		if (pojo.getBairro() != null || pojo.getCep() != null || pojo.getCidade() != null || pojo.getNumero() != null
+				|| pojo.getRua() != null || pojo.getUf() != null) {
+			String sqlEndereco = "INSERT INTO endereco(rua, bairro, numero, cidade, estado, cep) VALUES (?, ?, ?, ?, ?, ?) ";
+			try {
+				PreparedStatement stmt = dbConnection.prepareStatement(sqlEndereco);
+				stmt.setString(1, pojo.getRua());
+				stmt.setString(2, pojo.getBairro());
+				stmt.setString(3, pojo.getNumero());
+				stmt.setString(4, pojo.getCidade());
+				stmt.setString(5, pojo.getUf());
+				stmt.setString(6, pojo.getCep());
+				int retornoEnd = stmt.executeUpdate();
 
-			if (retornoEnd > 0) {
-				pojo.setId(ultimoID("endereco"));
+				if (retornoEnd > 0) {
+					pojo.setId(ultimoID("endereco"));
+				}
+
+				return retornoEnd > 0;
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+
+				return false;
 			}
-
-			return retornoEnd > 0;
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-			return false;
 		}
+		return false;
 	}
 
 	@Override
 	public boolean apagar(Endereco pojo) {
-		String sqlEndereco = "DELETE FROM endereco WHERE id =?";
-		try {
+		if (pojo.getId() != null) {
+			String sqlEndereco = "DELETE FROM endereco WHERE id =?";
+			try {
 
-			PreparedStatement cmdEndereco = dbConnection.prepareStatement(sqlEndereco);
-			cmdEndereco.setInt(1, pojo.getId());
-			int retorno = cmdEndereco.executeUpdate();
-			cmdEndereco.close();
+				PreparedStatement cmdEndereco = dbConnection.prepareStatement(sqlEndereco);
+				cmdEndereco.setInt(1, pojo.getId());
+				int retorno = cmdEndereco.executeUpdate();
+				cmdEndereco.close();
 
-			return retorno > 0;
+				return retorno > 0;
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
 		}
+		return false;
 	}
 
 	@Override
 	public boolean atualizar(Endereco novo) {
-		String sql = "UPDATE endereco SET rua = ?, bairro = ?, numero = ?, cidade = ?, estado = ?, cep = ? WHERE id = ?";
+		if (novo.getId() != null) {
+			String sql = "UPDATE endereco SET rua = ?, bairro = ?, numero = ?, cidade = ?, estado = ?, cep = ? WHERE id = ?";
 
-		try {
-			PreparedStatement cmd = dbConnection.prepareStatement(sql);
-			cmd.setString(1, novo.getRua());
-			cmd.setString(2, novo.getBairro());
-			cmd.setString(3, novo.getNumero());
-			cmd.setString(4, novo.getCidade());
-			cmd.setString(5, novo.getUf());
-			cmd.setString(6, novo.getCep());
-			cmd.setInt(7, novo.getId());
-			int retorno = cmd.executeUpdate();
-			cmd.close();
-			return retorno > 0;
+			try {
+				PreparedStatement cmd = dbConnection.prepareStatement(sql);
+				cmd.setString(1, novo.getRua());
+				cmd.setString(2, novo.getBairro());
+				cmd.setString(3, novo.getNumero());
+				cmd.setString(4, novo.getCidade());
+				cmd.setString(5, novo.getUf());
+				cmd.setString(6, novo.getCep());
+				cmd.setInt(7, novo.getId());
+				int retorno = cmd.executeUpdate();
+				cmd.close();
+				return retorno > 0;
 
-		} catch (SQLException e) {
-			e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			return false;
 		}
-
 		return false;
 	}
 
 	@Override
 	public List<Endereco> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public Endereco buscar(Integer id) {
-		String sql = "SELECT e.id, e.rua, e.bairro, e.numero, e.cidade, e.estado, e.cep, p.id, p.endereco_id FROM Endereco AS e, Pessoa as p WHERE p.id = '"
-				+ id + "' AND e.id = p.endereco_id";
+		ArrayList<Endereco> lista = new ArrayList<>();
+		String sql = "SELECT * FROM endereco";
 		Endereco endereco = null;
 		try {
 			Statement cmd = dbConnection.createStatement();
 			ResultSet rs = cmd.executeQuery(sql);
 			// enquanto houver um próximo registro, leia-os
 			while (rs.next()) {
-				int ids = rs.getInt("e.id");
-				String rua = rs.getString("e.rua");
-				String bairro = rs.getString("e.bairro");
-				String numero = rs.getString("e.numero");
-				String cidade = rs.getString("e.cidade");
-				String estado = rs.getString("e.estado");
-				String cep = rs.getString("e.cep");
+				int ids = rs.getInt("id");
+				String rua = rs.getString("rua");
+				String bairro = rs.getString("bairro");
+				String numero = rs.getString("numero");
+				String cidade = rs.getString("cidade");
+				String estado = rs.getString("estado");
+				String cep = rs.getString("cep");
 
 				endereco = new Endereco(ids, rua, bairro, numero, cidade, estado, cep);
+				lista.add(endereco);
 
 			}
 			rs.close();
@@ -117,26 +124,39 @@ public class EnderecoDAO extends AbstractGenericDAO<Endereco> {
 			e.printStackTrace();
 		}
 
-		return endereco;
+		return lista;
 	}
 
-	public Integer getEndereco(String id) {
-		String sql = "SELECT * FROM endereco WHERE id = " + id + "";
-		Integer idEndereco = null;
-		try {
-			Statement cmd = dbConnection.createStatement();
-			ResultSet rs = cmd.executeQuery(sql);
-			// enquanto houver um próximo registro, leia-os
-			while (rs.next()) {
-				idEndereco = rs.getInt("id");
+	public Endereco buscar(Integer id) {
+		if (id != null) {
+			String sql = "SELECT e.id, e.rua, e.bairro, e.numero, e.cidade, e.estado, e.cep, p.id, p.endereco_id FROM Endereco AS e, Pessoa as p WHERE p.id = '"
+					+ id + "' AND e.id = p.endereco_id";
+			Endereco endereco = null;
+			try {
+				Statement cmd = dbConnection.createStatement();
+				ResultSet rs = cmd.executeQuery(sql);
+				// enquanto houver um próximo registro, leia-os
+				while (rs.next()) {
+					int ids = rs.getInt("e.id");
+					String rua = rs.getString("e.rua");
+					String bairro = rs.getString("e.bairro");
+					String numero = rs.getString("e.numero");
+					String cidade = rs.getString("e.cidade");
+					String estado = rs.getString("e.estado");
+					String cep = rs.getString("e.cep");
 
+					endereco = new Endereco(ids, rua, bairro, numero, cidade, estado, cep);
+
+				}
+				rs.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			rs.close();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
+			return endereco;
 		}
-		return idEndereco;
+		return null;
 	}
 
 }
--- src/br/com/sistema/test/EnderecoTest.java
+++ src/br/com/sistema/test/EnderecoTest.java
@@ -1,5 +1,105 @@
 package br.com.sistema.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import br.com.sistema.conexao.Conexao;
+import br.com.sistema.model.Endereco;
+import br.com.sistema.model.dao.EnderecoDAO;
+
 public class EnderecoTest {
 
+    private static Conexao conexao = new Conexao("sistema_pet", "sistema_pet", "bd_pet486231");
+    private static EnderecoDAO enderecoDAO = new EnderecoDAO(conexao);
+    private static Integer enderecoId;
+
+    @BeforeAll
+    public static void testInserirEnderecoValido() {
+        Endereco endereco = new Endereco(null, "Rua ABC", "Bairro XPTO", "678", "Cidade A", "Estado AA", "12345-345");
+
+        boolean inserido = enderecoDAO.inserir(endereco);
+
+        assertTrue(inserido);
+        assertNotNull(endereco.getId());
+        enderecoId = endereco.getId();
+    }
+
+    @Test
+    public void testInserirEnderecoInvalido() {
+        Endereco endereco = new Endereco(null, null, null, null, null, null, null);
+
+        boolean sucesso = enderecoDAO.inserir(endereco);
+
+        assertFalse(sucesso);
+    }
+
+    @Test
+    public void testAtualizarEnderecoValido() {
+        Endereco endereco = new Endereco(enderecoId, "Rua ABC", "Bairro XPTO", "678", "Cidade A", "Estado AA", "12345-345");
+        enderecoDAO.inserir(endereco);
+        endereco.setRua("Nova Rua Alterada");
+
+        boolean atualizado = enderecoDAO.atualizar(endereco);
+
+        assertTrue(atualizado);
+    }
+
+    @Test
+    public void testAtualizarEnderecoInvalido() {
+        Endereco endereco = new Endereco(null, "Rua ABC", "Bairro XPTO", "678", "Cidade A", "Estado AA", "12345-345");
+
+        boolean atualizado = enderecoDAO.atualizar(endereco);
+
+        assertFalse(atualizado);
+    }
+
+    @Test
+    public void testBuscarEnderecoExistenteValido() {
+    	Integer pessoaId = 1;
+        Endereco enderecoEncontrado = enderecoDAO.buscar(pessoaId);
+
+        assertNotNull(enderecoEncontrado);
+    }
+
+    @Test
+    public void testBuscarEnderecoExistenteInvalido() {
+        Integer enderecoIdNaoExistente = 100000000;
+        Endereco enderecoEncontrado = enderecoDAO.buscar(enderecoIdNaoExistente);
+
+        assertNull(enderecoEncontrado);
+    }
+
+    @Test
+    public void testBuscarListaEnderecoValido() {
+        List<Endereco> enderecos = enderecoDAO.listarTodos();
+
+        assertNotNull(enderecos);
+    }
+
+    @Test
+    public void testApagarEnderecoInvalido() {
+        Endereco endereco = new Endereco(null, null, null, null, null, null, null);
+
+        boolean deletado = enderecoDAO.apagar(endereco);
+
+        assertFalse(deletado);
+    }
+
+    @AfterAll
+    public static void testApagarEnderecoValido() {
+        Endereco endereco = new Endereco(enderecoId, "Rua ABC", "Bairro XPTO", "678", "Cidade A", "Estado AA", "12345-345");
+
+        boolean deletado = enderecoDAO.apagar(endereco);
+
+        assertTrue(deletado);
+    }
 }
+
